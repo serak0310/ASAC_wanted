77437694109d4a060955633092caf242439e5039
Minjeong Kim <serak0310@naver.com>
2023-05-08 10:21:23 +0900

    add :  로그인 후 헤더 - 프로필 이미지 버튼

--- wanted_react/src/components/layout/LoggedUserIcon.js
+++ wanted_react/src/components/layout/LoggedUserIcon.js
@@ -4,7 +4,12 @@ import { ReactComponent as Bell_ic } from "../../assets/svg/ic_bell.svg";
 const LoggedUserIcon = () => {
   return (
     <>
-      <Bell_ic />
+      <div className="loggedUserIcon_wrap">
+        <Bell_ic />
+        <button className="header_profile_btn">
+          <img src={"https://static.wanted.co.kr/images/profile_default.png"}></img>
+        </button>
+      </div>
     </>
   );
 };
